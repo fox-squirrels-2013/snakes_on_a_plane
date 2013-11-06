6de253514c91760e4d6598a37448e425b462a54e
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-11-05 18:37:39 -0800

    changes color on click

---
+++ test.js
@@ -0,0 +1,16 @@
+var text = document.getElementsByTagName('p')[0].innerText
+
+console.log(text)
+
+var eachChar = text.split('')
+var spannedText = '';
+
+for(var i =0; i < eachChar.length; i++) {
+  spannedText += "<span onclick='changeColor(" + i + ")'>" + eachChar[i] + "</span>"
+}
+
+document.getElementsByTagName('p')[0].innerHTML = spannedText
+
+function changeColor(i) {
+  document.getElementsByTagName('span')[i].className = 'correct'
+}
