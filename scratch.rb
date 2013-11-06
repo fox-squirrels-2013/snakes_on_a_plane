be110993915a42e7d159fade3539e1386b36f67f
Apprentice <apprentice@dbc08.local>
2013-11-05 21:03:37 -0800

    Experiments.

---
+++ scratch.rb
@@ -0,0 +1,21 @@
+dummy_array = "Do you see any Teletubbies in here?".split('')
+
+user_input = ["D", "o", " ", "y", "o", "u", " ", "x", "s", "e", "e", " ", "a", "n", "y", " ", "T", "e", "l", "e", "t", "u", "b", "b", "i", "e", "s", " ", "i", "n", " ", "h", "e", "r", "e", "?", "y"]
+
+p dummy_array == user_input
+
+truthiness = true
+
+dummy_array.each_with_index do |letter, index|
+  if letter == user_input[index] 
+    truthiness = true
+  else
+    user_input.delete_at(index)
+    truthiness = false
+  end
+  truthiness = false if user_input.length > dummy_array.length
+end
+
+
+
+p truthiness
--- spike.js
+++ spike.js
@@ -4,4 +4,28 @@ function inputBox() {
   var testText = document.getElementById('dummy_text').innerHTML.split("").slice(0,userInput.length);
   document.getElementsByName('user_output')[0].value = (userInput.join('') == testText.join(''));
 
+  var truthiness = true;
+
+  var i = 0;
+  var x = 0;
+
+
+  // while (x <= userInput.length){
+  //   if (testText[i] === userInput[i]){
+  //     truthiness = true;
+  //     i += 1;
+  //     x = i;
+  //   }
+  //   else {
+  //     userInput.splice(i, 1);
+  //     truthiness = false;
+  //     i += 1;
+  //   }
+  //   if (userInput.length > testText.length) {
+  //     truthiness = false;
+  //   }
+  // }
+
+  document.getElementsByName('user_output')[0].value = truthiness;
+
 }
