71a32cfc87dc25b0c81be5598b06cacab64b97b3
Apprentice <apprentice@dbc07.local>
2013-11-06 18:43:13 -0800

    it goes word by word and displays if the word was typed correctly

--- spike2.js
+++ spike2.js
@@ -1,9 +1,28 @@
-function inputBox() {
+function inputBox(event) {
+  SPACEBAR_KEYCODE = 32;
   var pageText = document.getElementById("dummy_text").innerHTML;
+  var numWordsText = trackNumWords(pageText);
   var splitText = splitUpText(pageText);
-  // console.log(splitText)
+
+  var input = document.getElementsByName("user_input")[0].value;
+  var numWordsInput = trackNumWords(input);
+
+  console.log(compareText(input, splitText, numWordsInput));
 }
 
 function splitUpText(input_text) {
   return input_text.split(" ");
+}
+
+function trackNumWords(input) {
+  return splitUpText(input).length;
+}
+
+function compareText(input, splitText, numWordsInput) {
+  if (event.keyCode === SPACEBAR_KEYCODE) {
+    var wholeWordsTyped = splitUpText(input)[numWordsInput - 1];
+    var correspondingPageText = splitText[numWordsInput -1];
+
+    return wholeWordsTyped === correspondingPageText;
+  }
 }
