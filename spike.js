99d9922129e4890d4be3305845df1cd976d18c84
Apprentice <apprentice@dbc08.local>
2013-11-06 16:09:10 -0800

    Not working. Please check.

--- spike.js
+++ spike.js
@@ -1,13 +1,34 @@
 function inputBox() {
-  // document.getElementsByName('user_output')[0].value = document.getElementsByName('user_input')[0].value;
-  var userInput = document.getElementsByName('user_input')[0].value.split("");
-  var testText = document.getElementById('dummy_text').innerHTML.split("").slice(0,userInput.length);
-  document.getElementsByName('user_output')[0].value = (userInput.join('') == testText.join(''));
 
-  var truthiness = true;
+  // Gets dummy text and user text and splits them into arrays.
+  var userInput = document.getElementsByName('user_input')[0].value.split(" ");
+  var testText = document.getElementById('dummy_text_hidden').innerHTML.split(" ").slice(0,userInput.length);
 
-  var i = 0;
-  var x = 0;
+  // Gets the plain dummy text.
+  var dummyText = document.getElementById('dummy_text').innerText;
+
+
+  // Split plain dummy text to seperate array  and initialize spanned text.
+  var eachWord = dummyText.split(' ');
+  var spannedText = '';
+
+  // Create spanned version of dummy text. (with span tags)
+  for(var index =0; index < eachWord.length; index++) {
+    spannedText += "<span onclick='changeColor(" + index + ")'>" + eachWord[index] + "</span>";
+  }
+
+
+  // Replace original dummy text on document with spanned version.
+  document.getElementById('dummy_text')[0].innerHTML = spannedText;
+
+  function changeColor(i) {
+    document.getElementsByTagName('span')[i].className = 'correct';
+  }
+
+  // var truthindexness = true;
+
+  // var i = 0;
+  // var x = 0;
 
 
   // while (x <= userInput.length){
@@ -26,6 +47,9 @@ function inputBox() {
   //   }
   // }
 
-  document.getElementsByName('user_output')[0].value = truthiness;
+
+
+
+  // document.getElementsByName('user_output')[0].value = truthiness;
 
 }
